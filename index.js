2534117613d80062c75163038b70d941b3c6c954
Rob <alekseicruz@gmail.om>
2020-03-08 23:23:31 -0700

    implemented data parsing

--- index.js
+++ index.js
@@ -21,3 +21,42 @@
 // Questions
 // User GitHub profile picture
 // User GitHub email
+
+// The user will be prompted for their GitHub username, which will be used to make a call to the GitHub API
+// to retrieve their email and profile image. They will then be prompted with questions about their project.
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+getGithubUserData();
+
+
+async function getGithubUserData() {
+    try {
+        const { user } = await inquirer.prompt({
+            message: "Enter your Github username:",
+            name: "user"
+        });
+
+        console.log('username', user)
+        const { data } = await axios.get(`https://api.github.com/users/${user}/events/public`);
+
+
+        // console.log(config);
+        const array = [];
+        let avatarUrl;
+        data.forEach((element, index) => {
+            if (index === 0) { avatarUrl = element.actor.avatar_url }
+            let repo = element.repo.name.split("/").pop();
+            if (!array.includes(repo)) { array.push(repo) }
+
+        });
+        // console.log("===================================================================================== end")
+
+        console.log("array", array);
+        console.log("avatarUrl", avatarUrl);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
