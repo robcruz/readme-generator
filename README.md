3810497752e5c27af5a743e70b1d7f280f5b86fb
Rob <alekseicruz@gmail.com>
2020-03-09 17:21:46 -0700

    initial

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# readme-generator
+# Rob Cruz
+![Rob Cruz](https://avatars0.githubusercontent.com/u/3228849?v=4)
+#### URL: [https://github.com/robcruz](https://github.com/robcruz)
+#### Username: robcruz
+#### Github Repositories:
+
--- index.js
+++ index.js
@@ -24,45 +24,135 @@
 
 // The user will be prompted for their GitHub username, which will be used to make a call to the GitHub API
 // to retrieve their email and profile image. They will then be prompted with questions about their project.
+
 const axios = require("axios");
 const inquirer = require("inquirer");
+const fs = require("fs");
 
-getGithubData();
+let readMe = "";
+let username = 'robcruz';
+console.log('username', username);
 
-async function getGithubData() {
-    try {
-        // const { username } = await inquirer.prompt({
-        //     message: "Enter your Github username:",
-        //     name: "username"
-        // });
+try {
+    // const { username } = await inquirer.prompt({
+    //     message: "Enter your Github username:",
+    //     name: "username"
+    // });
 
-        let username = 'robcruz';
-        console.log('username', username);
-        const { data } = await axios.get(`https://api.github.com/users/${username}/events/public`);
 
-        const array = [];
-        let avatarUrl;
-        let email;
+    axios.get(`https://api.github.com/users/${username}`)
+        .then(response => {
+            // console.log('response.data)', response.data);
+            readMe += `# ${response.data.name}\n`;
+            readMe += `![${response.data.name}](${response.data.avatar_url})\n`;
+            readMe += `#### URL: [${response.data.html_url}](${response.data.html_url})\n`;
+            readMe += `#### Username: ${response.data.login}\n`;
+            if (response.data.company) {
+                readMe += `#### Company: ${response.data.company}\n`;
+            }
+            if (response.data.email) {
+                readMe += `#### Email: ${response.data.email}\n`;
+            }
+            readMe += `#### Github Repositories:\n`;
 
-        data.forEach((element) => {
-            let { actor, repo, payload } = element;
-
-            if (!email) email = payload.commits[0].author.email;
-
-            if (!avatarUrl) avatarUrl = actor.avatar_url;
-
-            let repoName = repo.name.split("/").pop();
-            if (!array.includes(repoName)) { array.push(repoName) }
-
+            addNewReadme(readMe);
+            console.log(readMe);
         });
-        // console.log("===================================================================================== end")
 
-        console.log("array", array);
-        console.log("avatarUrl", avatarUrl);
-        console.log("email", email);
+    axios.get(`https://api.github.com/users/${username}/repos`)
+        .then(response => {
+            console.log('response.data', response.data);
 
-    } catch (err) {
-        console.log(err);
-    }
+
+            // readMe += `# ${data.name}\n`;
+            // readMe += `![${data.name}](${data.avatar_url})\n`;
+            // readMe += `#### URL: [${data.html_url}](${data.html_url})\n`;
+            // readMe += `#### Github Username: ${data.login}\n`;
+            // // readMe += `#### Email: ${email}\n`;
+            // readMe += `#### Github Repositories:\n`;
+            //
+            // addNewReadme(readMe);
+            // console.log(readMe);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+} catch (err) {
+    console.log(err);
 }
 
+
+
+function addNewReadme(str) {
+    fs.writeFile("README.md", `${str}\n`, err => {
+        if (err) {
+            console.log(err);
+        }
+    })
+}
+
+function appendToReadMe(str) {
+    fs.appendFile("README.md", `${str}\n`, err => {
+        if (err) {
+            console.log("\nThere was an error!\n")
+            console.log(err);
+        }
+    })
+}
+
+
+
+
+// axios.get(`https://api.github.com/users/${username}`)
+//     .then(response => {
+//         let data = response.data;
+//         readMe += `# ${data.name}\n`;
+//         readMe += `![${data.name}](${data.avatar_url})\n`;
+//         readMe += `#### URL: [${data.html_url}](${data.html_url})\n`;
+//         readMe += `#### Github Username: ${data.login}\n`;
+//         // readMe += `#### Email: ${email}\n`;
+//         readMe += `#### Github Repositories:\n`;
+//
+//         fs.writeFile("README.md", readMe, err => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//         })
+//
+//         console.log(readMe);
+//     })
+
+
+
+
+
+// const { data } = await axios.get(`https://api.github.com/users/${username}`);
+
+// const { data } = await axios.get(`https://api.github.com/users/${username}/events/public`);
+//
+// const repos = [];
+// let name;
+// let avatarUrl;
+// let email;
+// let readMe = "";
+//
+// data.forEach((element) => {
+//     let { actor, repo, payload } = element;
+//     if (!name) name = payload.commits[0].author.name;
+//     if (!email) email = payload.commits[0].author.email;
+//     if (!avatarUrl) avatarUrl = actor.avatar_url;
+//     let repoName = repo.name.split("/").pop();
+//     if (!repos.includes(repoName)) { repos.push(repoName) }
+//
+// });
+//
+// readMe += `# ${name}\n`;
+// readMe += `![${username}](${avatarUrl})\n`;
+// readMe += `#### Github Username: ${username}\n`;
+// readMe += `#### Email: ${email}\n`;
+// readMe += `#### Github Repositories:\n`;
+
+// repos.forEach(repo => {
+//     readMe += `* ${repo}\n`;
+// })
